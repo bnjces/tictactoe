b5b2b84cd3bde8e752bd3968610ee05c83c8e2db
bnjces <bnjces@gmail.com>
2021-01-26 21:50:59 -0500

    Rows are checked for victory condition.

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ class Game {
     static XPLAYER = "X";                   // create strings for X, O, blank
     static OPLAYER = "O";                   // static constants are all caps
     static EMPTY = " ";
+    static gameOver = false;
 
     static board= [                         // make the game board, an array
         this.EMPTY, this.EMPTY, this.EMPTY,
@@ -11,19 +12,56 @@ class Game {
     ]    
     
     static xPlays(e){                       // logs click (and X) to array element
+        if(this.gameOver){
+            return;
+        }
+        
+        Display.setMessage("&nbsp;");
         let pos = e.srcElement.id;
         console.log(pos);
+            if(this.board[pos] !== this.EMPTY){  // ensures that X can only be placed in a blank spot
+                Display.setMessage ("Your mother smells of elderberries. Pick an empty spot.")
+                return;
+            }
         this.board[pos] = this.XPLAYER;
+        if(this.didXWin()){                 // check if X won, and display message
+            Display.boardRefresh();
+            Display.setMessage("Congratulations! You win!");
+            this.gameOver = true;
+            return;
+            }
         this.oPlays();
         Display.boardRefresh();             // tell the display to update                      
     }
 
+    static didXWin() {                      // checks whether the human player has won
+        let win = true;
+        // check the rows:
+        for (let i = 0; i < Game.board.length; i += 3){
+            win = true;
+            for (let j = 0; j < 3; j++) {
+                if(Game.board[i + j] !== this.XPLAYER){
+                    win = false;
+                    break;
+                }
+            }
+            if(win){
+                return win;
+            } 
+        }
+        // check the columns:
+        
+        // check the diagonals:
+
+        // output an outcome message of win, loss, or tie:
+    }
 
     static reset(){                         // makes the New Game button clear the board
         for(let i = 0; i < Game.board.length; i++){
             this.board[i] = this.EMPTY;     // resets board
         }
         Display.boardRefresh();             // displays the reset board
+        this.gameOver = false;
 
     }
 
