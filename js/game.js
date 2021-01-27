c4f804f7f58df8256c10e9c7eea1b7dbb76b74c3
bnjces <bnjces@gmail.com>
2021-01-26 19:55:32 -0500

    Early game logic.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,20 @@
 class Game {
 
-    .
+    static XPLAYER = "X";   // create strings for X, O, blank
+    static OPLAYER = "O";   // static constants are all caps
+    static EMPTY = " ";
 
+    static board= [         // make the game board, an array
+        this.EMPTY, this.EMPTY, this.EMPTY,
+        this.EMPTY, this.EMPTY, this.EMPTY,
+        this.EMPTY, this.EMPTY, this.EMPTY
+    ]    
+    
+    static xPlays(e){
+        let pos = e.srcElement.id;
+        console.log(pos);
+        this.board[pos] = this.XPLAYER;
+    }
 
 
 
