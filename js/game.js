591f82f2ff0119e1765f88bd62c4a1df69cca0c6
bnjces <bnjces@gmail.com>
2021-01-26 20:57:47 -0500

    X Input now works fully.

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 class Game {
 
-    static XPLAYER = "X";   // create strings for X, O, blank
-    static OPLAYER = "O";   // static constants are all caps
+    static XPLAYER = "X";                   // create strings for X, O, blank
+    static OPLAYER = "O";                   // static constants are all caps
     static EMPTY = " ";
 
-    static board= [         // make the game board, an array
+    static board= [                         // make the game board, an array
         this.EMPTY, this.EMPTY, this.EMPTY,
         this.EMPTY, this.EMPTY, this.EMPTY,
         this.EMPTY, this.EMPTY, this.EMPTY
@@ -14,7 +14,9 @@ class Game {
         let pos = e.srcElement.id;
         console.log(pos);
         this.board[pos] = this.XPLAYER;
+        Display.boardRefresh();             // tell the display to update
     }
+
 
 
 
