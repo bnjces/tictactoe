1fdd245ad66fbd70ff4b7aee63b47db1d4743fa9
bnjces <bnjces@gmail.com>
2021-01-26 21:17:34 -0500

    Computer now plays 'O,' basic functionality.

--- js/game.js
+++ js/game.js
@@ -10,15 +10,32 @@ class Game {
         this.EMPTY, this.EMPTY, this.EMPTY
     ]    
     
-    static xPlays(e){
+    static xPlays(e){                       // logs click (and X) to array element
         let pos = e.srcElement.id;
         console.log(pos);
         this.board[pos] = this.XPLAYER;
-        Display.boardRefresh();             // tell the display to update
+        this.oPlays();
+        Display.boardRefresh();             // tell the display to update                      
     }
 
 
+    static reset(){                         // makes the New Game button clear the board
+        for(let i = 0; i < Game.board.length; i++){
+            this.board[i] = this.EMPTY;     // resets board
+        }
+        Display.boardRefresh();             // displays the reset board
+
+    }
+
+    static oPlays(){                        // create oPlays method for Computer play
+        for(let i = 0; i < Game.board.length; i++){
+            if(this.board[i] === this.EMPTY){ // finds empty spot in board to make a play
+                this.board[i] = this.OPLAYER; // places an O in that empty spot
+                return;                       // stop looking for more empty spaces else it will continue forever
+            }
+        }
+    }
+}   
 
 
 
-}
