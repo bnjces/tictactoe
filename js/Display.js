af4c3e32ebbfce4f4cdecac856422ab4aeec6ed4
bnjces <bnjces@gmail.com>
2021-01-26 20:35:12 -0500

    Added Display.js and default.js.

---
+++ js/Display.js
@@ -0,0 +1,9 @@
+class Display  {  // Code for the Display below the game, so the player is aware of outcomes.
+
+    static setMessage (message){
+        document.getElementById("message").innerHTML = message;  // retrieve msg below the board
+    }  
+
+
+
+} 
